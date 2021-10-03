41074b1a62de7a9d58539323697fc2e9c0ffdead
irwandelaw15 <irwandelaw15@gmail.com>
2021-10-03 22:41:46 +0700

    contoh1,contoh2,contoh3, dan menghilangkan index.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,12 +1,12 @@
-<?php
-class Contoh1 extends CI_Controller
-{
+<?php 
+class contoh1 extends CI_Controller {
     public function index()
     {
-        echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Irwan Telaumbanua
-        Saya tingga di daerah Kelapa Gading
-        olah raga yang saya sukai adalah
-        Bulutangkis";
+        echo"<h1>Perkenalkan</h1>";
+        echo"Irwan Telaumbanua <br>
+        Saya tinggal di daerah Kelapa Gading - Jakarta Utara <br>
+        Olahraga yang saya sukai adalah bulutangkis"; 
     }
 }
+
+?>
