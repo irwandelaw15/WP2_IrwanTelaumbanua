dd433286e633f943a1f1f531c79a43045103855e
irwandelaw15 <irwandelaw15@gmail.com>
2021-10-13 23:39:32 +0700

    MEMBUAT TEMPLATE (TEMPLATING) WEB SEDERHANA

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Irwan</li>
+ <li>Nama Belakang : Telaumbanua</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kelapa Gading - Summarecon Mall</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bawolato ( Nias ) </li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
